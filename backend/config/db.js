7167524bccf297006c4a7a0b668e6530baf764e1
vijay2219 <vijaykumar221918@gmail.com>
2026-02-14 13:52:08 +0530

    Updated backend changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const uri = 'mongodb://localhost:27017/hostel'; // Hard-coded MongoDB URI
-    const conn = await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const uri = process.env.MONGO_URI;
+
+    const conn = await mongoose.connect(uri);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,6 @@ import complaints from './routes/complaints.js';
 import studentRoutes from './routes/students.js';
 import gatePass from './routes/gatePass.js';
 
-
-
-const PORT = 5000;
 const app = express();
 
 connectDB();
@@ -17,11 +14,13 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/foodMenu',foodMenuRoutes);
-app.use('/api/complaints',complaints);
-app.use('/api/students',studentRoutes);
-app.use('/api/gatePass',gatePass);
+app.use('/api/foodMenu', foodMenuRoutes);
+app.use('/api/complaints', complaints);
+app.use('/api/students', studentRoutes);
+app.use('/api/gatePass', gatePass);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
